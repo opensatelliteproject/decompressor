e42feffc5f81c3fadc7f6ca77d27f82237341618
Lucas Teske <lucas@teske.net.br>
2017-02-22 21:07:04 -0300

    Added MSVC Solution

--- decompresslib.h
+++ decompresslib.h
@@ -1,6 +1,13 @@
 #include <szlib.h>
 
+#ifdef _WIN32
+#define LIBSATDECOMPRESS_API __declspec(dllexport)
+#define size_t unsigned int
+#else
+#define LIBSATDECOMPRESS_API
+#endif
+
 #ifndef __DECOMPRESS_LIB__
 #define __DECOMPRESS_LIB__
-extern int LritRiceDecompress(char *input, char *output, size_t inputLength, size_t outputLength, int bitsPerPixel, int pixelsPerBlock, int pixelsPerScanline, int mask);
+extern int LIBSATDECOMPRESS_API LritRiceDecompress(char *input, char *output, size_t inputLength, size_t outputLength, int bitsPerPixel, int pixelsPerBlock, int pixelsPerScanline, int mask);
 #endif
